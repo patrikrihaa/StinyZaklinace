913d63df21e6038e5aea2f88546cd37631360e3f
Patrik Říha <dogyczsk@patrik--macbook-pro.local>
2026-01-19 20:33:32 +0100

    JSON

--- src/game/Location.java
+++ src/game/Location.java
@@ -1,4 +1,57 @@
 package game;
 
+import java.util.ArrayList;
+
 public class Location {
+    private String id;
+    private String name;
+    private String description;
+    private ArrayList<String> nextLocations;
+    private ArrayList<String> Items;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<String> getNextLocations() {
+        return nextLocations;
+    }
+
+    public void setNextLocations(ArrayList<String> nextLocations) {
+        this.nextLocations = nextLocations;
+    }
+
+    public ArrayList<String> getItems() {
+        return Items;
+    }
+
+    public void setItems(ArrayList<String> items) {
+        Items = items;
+    }
+
+    @Override
+    public String toString() {
+        return  name + " - " + description;
+    }
 }
+
