db586637b0152eb70429b47db4803b5f3617a779
Patrik <patrik.rihaa@gmail.com>
2026-02-22 13:59:22 +0100

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 import game.Game;
 
+
+/**
+ * Spouští smyčku hry Stíny Zaklínače.
+ *
+ * @author Patrik Říha
+ */
+
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
